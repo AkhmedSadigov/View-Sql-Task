dfce3a6ccbbd8a59f36c2ce776a1b5a8edad54ef
ahmed <ahmed@LAPTOP-D0D6I3G9>
2022-11-26 16:35:02 +0400

    Add project files.

---
+++ View Sql Task/View.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="View.aspx.cs" Inherits="View_Sql_Task.View" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<link href="style.css" rel="stylesheet" />
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl1" runat="server" Text="Category Name"></asp:Label>
+            <asp:TextBox ID="txtbox1" runat="server" OnTextChanged="txtbox1_TextChanged"></asp:TextBox>
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server" CssClass="mydatagrid" PagerStyle-CssClass="pager"
+ HeaderStyle-CssClass="header" RowStyle-CssClass="rows" ></asp:GridView>
+        </div>
+        <div>
+            <asp:Label ID="lbl2" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="btn1" runat="server" CssClass="button" Text="Search" OnClick="btn1_click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ View Sql Task/View.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace View_Sql_Task
+{
+    public partial class View : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn1_click(object sender, EventArgs e)
+        {
+            SqlConnection SQLConn = new SqlConnection("Data Source=LAPTOP-D0D6I3G9\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            SQLConn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from View_C#task where CategoryName='" + txtbox1.Text + "'", SQLConn);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable tb = new DataTable();
+            adapter.Fill(tb);
+
+            if (tb.Rows.Count > 0)
+            {
+                GridView1.DataSource = tb;
+                GridView1.DataBind();
+                lbl2.Text = "";
+            }
+            else
+            {
+                lbl2.Text = "Not Found";
+                GridView1.DataSource = null;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void txtbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
